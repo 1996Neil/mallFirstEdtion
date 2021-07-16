ec951678a1e04d9bee20fc65072224d6f2d4a7ca
Neil <wangzhecoc@gmail.com>
2021-07-16 22:29:50 +0800

    SpringBoot+Mybatis 7/16 22:30

---
+++ src/main/java/com/mall/mallfirstedtion/mbg/CommentGenerator.java
@@ -0,0 +1,11 @@
+package com.mall.mallfirstedtion.mbg;
+
+/**
+ * @author wangzhe
+ * @ClassName CommentGenerator
+ * @create 2021-07-16 22:11
+ * @description
+ * @Version 1.0
+ */
+public class CommentGenerator {
+}
