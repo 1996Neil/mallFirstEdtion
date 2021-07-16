013b904bb40af362a1d386993d44354bde3e086c
Neil <wangzhecoc@gmail.com>
2021-07-16 21:43:00 +0800

    firstdemo7/16 21:40

---
+++ src/main/java/com/mall/mallfirstedtion/MallFirstEdtionApplication.java
@@ -0,0 +1,13 @@
+package com.mall.mallfirstedtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFirstEdtionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallFirstEdtionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mall/mallfirstedtion/MallFirstEdtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.mallfirstedtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallFirstEdtionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
