8c6ee2e768daa60cdadcc4ef5346b829406fad7a
Neil <wangzhecoc@gmail.com>
2021-07-16 23:18:24 +0800

    SpringBoot+Mybatis 7/16 23:20 完成了mybatis自动代码生成器

--- src/main/java/com/mall/mallfirstedtion/MallFirstEdtionApplication.java
+++ src/main/java/com/mall/mallfirstedtion/MallFirstEdtionApplication.java
@@ -2,7 +2,12 @@ package com.mall.mallfirstedtion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @Description: 启动类
+ * @Date 22:30 2021/7/16
+ * @Param no
+ * @return no
+ **/
 @SpringBootApplication
 public class MallFirstEdtionApplication {
 
--- src/main/java/com/mall/mallfirstedtion/mbg/CommentGenerator.java
+++ src/main/java/com/mall/mallfirstedtion/mbg/CommentGenerator.java
@@ -1,11 +1,68 @@
 package com.mall.mallfirstedtion.mbg;
 
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
 /**
  * @author wangzhe
  * @ClassName CommentGenerator
  * @create 2021-07-16 22:11
- * @description
+ * @description生成注释
  * @Version 1.0
  */
-public class CommentGenerator {
+public class CommentGenerator extends DefaultCommentGenerator {
+
+    private boolean addRemarkComments = false;
+
+    /**
+     * @return void
+     * @Description: 设置用户配置的参数
+     * @Date 22:52 2021/7/16
+     * @Param [properties]
+     **/
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    /**
+     * @return void
+     * @Description: 给字段添加注释
+     * @Date 22:54 2021/7/16
+     * @Param [field, introspectedTable, introspectedColumn]
+     **/
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        //根据参数和备注信息判断是否添加备注信息
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            addFieldJavaDoc(field, remarks);
+        }
+
+    }
+
+    /**
+     * @return void
+     * @Description: 给model字段添加注释, 给上面的方法做引用
+     * @Date 23:03 2021/7/16
+     * @Param [field, remarks]
+     **/
+    private void addFieldJavaDoc(Field field, String remarks) {
+        //文档注释开始
+        field.addJavaDocLine("/**");
+        //获取数据库字段的备注信息
+        String[] remarkLines = remarks.split(System.getProperty("line separator"));
+        for (String remarkLine : remarkLines) {
+            field.addJavaDocLine(" * " + remarkLine);
+        }
+        addJavadocTag(field, false);
+        field.addJavaDocLine(" */");
+
+    }
 }
